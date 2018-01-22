4f64582f9e2bd436c89f9b6614a6dad7709cb56c
TheIndianProgrammer <desaiv@umich.edu>
2018-01-22 10:22:09 -0500

    Homework 1 Done

--- CPS161 advanced JAVA/Projects/JH1_ReadingWithExceptions/Reading_With_Exceptions.java
+++ CPS161 advanced JAVA/Projects/JH1_ReadingWithExceptions/Reading_With_Exceptions.java
@@ -4,7 +4,11 @@ import java.util.*;
 
 public class Reading_With_Exceptions {
 
-	static void process(String inputFilename) {
+	static void process(String inputFilename) throws FileNotFoundException {
+		FileInputStream in_File = null;
+		Scanner reader = null;
+		PrintStream out = null;
+		PrintStream err = new PrintStream(new File("Errors.txt"));
 		
 		try{
 			//variables
@@ -12,32 +16,59 @@ public class Reading_With_Exceptions {
 			int numbers_to_read = -1;
 			
 			//Process
-			File input = new File(inputFilename);
-			Scanner reader = new Scanner(input);
+			in_File = new FileInputStream(inputFilename);
+			reader = new Scanner(in_File);
 			if(reader.hasNext())
 				outputFilename = reader.next();
-			else
-				System.out.println("Invalid File Format");
-			PrintStream out = new PrintStream(new File (outputFilename));
-			while(reader.hasNextInt())
+			else	
+				System.out.println("Invalid File Format!");
+			out = new PrintStream(new File(outputFilename));
+			if(reader.hasNextInt()) {
 				numbers_to_read = reader.nextInt();
-			if(numbers_to_read < 0) {
-				System.out.println("Invalid number given as the number of reading ints.");
+				if(numbers_to_read < 0) {
+					out.println("Bad int : numbers_to_read < 0");
+				}
 			}
-			copyNumbers(reader, out, numbers_to_read);
+			else{
+				out.println("[Bad input for numbers_to_read!("+ reader.next() +")]");
+			}
+			copyNumbers(reader, out, numbers_to_read, err);
 			printToScreen(outputFilename);
-			out.close();
 		}
 		catch(FileNotFoundException e) {
-			System.out.println( inputFilename + " does not exist in the given path.");
+			System.out.println( inputFilename + " does not exist in the given path.\n");
+		}
+		catch(InputMismatchException e) {
+			err.println(reader.next());
+		}
+		finally {
+			if(reader != null)
+				reader.close();
+			if(out != null)
+				out.close();
+			if(err != null)
+				err.close();
 		}
 	}
 	
-	static void copyNumbers(Scanner reader, PrintStream out, int num_int) {
-		while(reader.hasNextInt()) {
-			out.print(reader.next());
+	static void copyNumbers(Scanner reader, PrintStream out, int int_num, PrintStream err) {
+		int count = 0;
+		while(reader.hasNext() && count != int_num) {
+			if(count != 0 && count%10 == 0)
+				out.print(" \n");
+			if(reader.hasNextInt())
+				out.print(" " + reader.nextInt());
+			else {
+				out.print(" [Bad_Intput!(" + reader.next() + ")]");
+			}
+			count++;
 		}
-		reader.close();
+		out.print("\n");
+		if(int_num != -1 && count < int_num) {
+			out.println(" [Insufficient Data!]\n");
+		}
+		else
+			out.println();
 	}
 	
 	static void printToScreen(String filename){
@@ -60,12 +91,10 @@ public class Reading_With_Exceptions {
 		}
 	}// end of printToScreen
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException{
 		for(int i = 0; i < args.length; i++) {
 			System.out.println("Processing " + args[i] + "...");
 			process(args[i]);
 		}
 	}
-
 }

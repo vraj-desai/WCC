cbd86192b8e91817d31ec754db4db95a414c2352
TheIndianProgrammer <desaiv@umich.edu>
2018-02-02 10:45:10 -0500

    JH2 almost done

--- CPS161 advanced JAVA/Projects/JH2_@00655781/file_operations/FileOperations.java
+++ CPS161 advanced JAVA/Projects/JH2_@00655781/file_operations/FileOperations.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class FileOperations {
 	StringTokenizer parseCommand;
-	File currFile;
 	
 	public void delete(){
 		//		code for handling the delete command
@@ -29,80 +28,137 @@ public class FileOperations {
 		//		code for handling the rename command
 		//		Make sure you check the return code from the
 		//		File rename method to print out success/failure status
+		String fileName = parseCommand.nextToken();
+		File oldFile = new File(fileName);
+		if(oldFile.exists()){
+			String fileRename = parseCommand.nextToken();
+			File newFile = new File(fileRename);
+			oldFile.renameTo(newFile);
+		}
+		else {
+			System.out.println("The old file does not exist!");
+		}
 	}
 
 	public void list(){
 		// code for handling the list command
+		String dirName = parseCommand.nextToken();
+		File dir = new File(dirName);
+		if(dir.exists()) {
+			File[] dirFiles = dir.listFiles();
+			for(File currFile : dirFiles){
+				if(currFile.isFile()) {
+					System.out.println(currFile.getName());
+				}
+			}
+		}
+		else {
+			System.out.println("Directory to get file does not exist!");
+		}
 	}
 	
 	public void size(){
 		// code for handling the size command
+		String fileName = parseCommand.nextToken();
+		File inFile = new File(fileName);
+		if(inFile.exists()) {
+			System.out.println(inFile.length());
+		}
+		else {
+			System.out.println("File with a given name for a size is invalid!");
+		}
 	}
 	
 	public void lastModified(){
 		// code for handling the lastModified command
+		String fileName = parseCommand.nextToken();
+		File inFile = new File(fileName);
+		if(inFile.exists()) {
+			long lastDate = inFile.lastModified();
+			System.out.println(lastDate);
+		}
+		else {
+			System.out.println("File to check the lastmodified date with given name does not exist!");
+		}
 	}
 	
-	public void mkdir(String line){
+	public void mkdir(){
 		//		code for handling the mkdir command
 		//		Make sure you check the return code from the
 		//File mkdir method to print out success/failure status
 		String dirName = parseCommand.nextToken();
-		File dir = new File(dirName);
+		File newDir = new File(dirName);
+		if(!newDir.exists()) {
+			boolean result = newDir.mkdirs();
+			if(result == true) {
+				System.out.println("new directory with " + dirName + " created successfully!");
+			}
+			else {
+				System.out.println("Could not create new directory!");
+			}
+		}
+		else {
+			System.out.println("Directory with given name already exists!");
+		}
 	}
 	
-	public void createFile(String line){
+	public void createFile(){
 		// code for handling the createFile command
 		String fileName = null;
+		PrintStream print = null;
+		FileOutputStream outstream = null;
 		try {
 			fileName = parseCommand.nextToken();
-			File newFile = new File(fileName);
-			FileOutputStream outstream = new FileOutputStream(fileName);
-			PrintStream print = new PrintStream(outstream);
+			outstream = new FileOutputStream(fileName);
+			print = new PrintStream(outstream);
 			while(parseCommand.hasMoreTokens()) {
 				String writeData = parseCommand.nextToken();
 				print.println(writeData);
 			}
+			System.out.println("New file created and data written successfully!");
+			
 		}
 		catch(FileNotFoundException e) {
 			System.out.println("You are trying to create an outputstream to nonexisting file named :" + fileName);
 		}
-	}
-
-	public void printFile(String line){
-		// code for handling the printFile command
-		String fileName = parseCommand.nextToken();
-		FileInputStream readFile = null;
-		Scanner scanFile = null;
-		String outFileName = null;
-		if(parseCommand.hasMoreTokens()) {
-			outFileName = parseCommand.nextToken();
-		}
-		else {
-			System.out.println("No output file name given!");
-			return;
-		}
-		File newFile = new File(outFileName);
-		FileOutputStream outStream = null;
-		PrintStream print = null;
-				
-		try {
-			readFile = new FileInputStream(fileName);
-			scanFile = new Scanner(readFile);
-			outStream = new FileOutputStream(outFileName);
-			print = new PrintStream(outStream);
-			while(scanFile.hasNext()) {
-				print.println(scanFile.next());
+		finally {
+			if(print != null) {
+				print.close();
+			}
+			if(outstream != null) {
+				try {
+					outstream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					System.out.println("outputstream does not exist!");
+				}
 			}
 		}
-		catch(FileNotFoundException e) {
-			System.out.println("Tring to print non existing file named: " + fileName);
-			
+	}
+
+	public void printFile(){
+		// code for handling the printFile command
+		while(parseCommand.hasMoreTokens()) {
+			String fileName = parseCommand.nextToken();
+			FileInputStream readFile = null;
+			Scanner scanFile = null;
+			try {
+				readFile = new FileInputStream(fileName);
+				scanFile = new Scanner(readFile);
+				while(scanFile.hasNext()) {
+					System.out.println(scanFile.next());
+				}
+			}
+			catch(FileNotFoundException e) {
+				System.out.println("Tring to print non existing file named: " + fileName);
+				
+			}
 		}
 	}
 
 	void printUsage(){
 		// process the "?" command
+		System.out.print("?\ndelete filename\nrename oldFilename newFilename\nsize filename\nlastModified filename\nlist dir\nprintFile filename\ncreateFile filename\nmkdir dir\nquit");
 	}
 
 	//		useful private routine for getting next string on the command line
@@ -124,10 +180,6 @@ public class FileOperations {
 		return f;
 	}
 	
-	private void printhelpcmd() {
-		System.out.print("?\ndelete filename\nrename oldFilename newFilename\nsize filename\nlastModified filename\nlist dir\nprintFile filename\ncreateFile filename\nmkdir dir\nquit");
-	}
-	
 	public boolean processCommandLine(String line){
 		//		if line is not null, then setup the parseCommand StringTokenizer. Pull off the first string
 		//		using getNextToken() and treat it as the "command" to be processed.
@@ -141,42 +193,49 @@ public class FileOperations {
 			switch(cmd) {
 			case("?"):{
 				System.out.println("Processing: ?");
+				printUsage();
 				break;
 			}
 						
 			case("createFile"):{
 				System.out.println("Processing: createFile");
+				createFile();
 				break;
 			}
 			
 			case("printFile"):{
 				System.out.println("Processing: printFile");
+				printFile();
 				break;
 			}
 			
 			case("lastModified"):{
 				System.out.println("Processing: lastModified");
+				lastModified();
 				break;
 			}
 			
 			case("size"):{
 				System.out.println("Processing: size");
+				size();
 				break;
 			}
 			
 			case("rename"):{
 				System.out.println("Processing: rename");
+				rename();
 				break;
 			}
 			
 			case("mkdir"):{
 				System.out.println("Processing: mkdir");
-				mkdir(line);
+				mkdir();
 				break;
 			}
 			
 			case("delete"):{
 				System.out.println("Processing: delete");
+				delete();
 				break;
 			}
 			
@@ -187,6 +246,7 @@ public class FileOperations {
 			
 			case("quit"):{
 				System.out.println("Processing: quit");
+				list();
 				break;
 			}
 			

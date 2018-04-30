2729b8621a8f879a4ad8b883ee73a3c40fb82d4e
TheIndianProgrammer <desaiv@umich.edu>
2018-04-30 12:39:33 -0400

    CIS161 completed

---
+++ CIS161 Intro To PowerShell/final/get-maxfile.ps1
@@ -0,0 +1,29 @@
+﻿param([Parameter(Mandatory = $true, position = 0)]$path)
+
+if(-not(test-path $path)){
+    Write-Host "Invalid path!" -ForegroundColor Red
+    exit
+}
+
+$dirCheck = (Get-Item $path).PSIsContainer
+
+if(-not$dirCheck){
+    Write-Host "$path is not directory!" -ForegroundColor Red
+    exit
+}
+
+$numFiles = 0
+$largestFileSize = 0
+$largestFileName = $null
+$pathInfo = Get-ChildItem $path -Recurse
+
+foreach($file in $pathInfo){
+    $numFiles++
+    if($file.Length -gt $largestFileSize){
+        $largestFileName = $file.Name
+        $largestFileSize = $file.length
+    }
+}
+
+Write-Host "There are $numFiles files in $path"
+Write-Host "Largest file is $largestFileName with a size of $largestFileSize"
---
+++ CIS161 Intro To PowerShell/final/get-spaceinfo.ps1
@@ -0,0 +1,21 @@
+﻿$volumeInfo = Get-WmiObject win32_volume
+$totalCapacity = 0
+$totalFree = 0
+[String]$drives = $null
+
+foreach($volume in $volumeInfo){
+    Write-Host $volume.DriveLetter
+    $letter = $volume.DriveLetter
+    $fileSystem = $volume.FileSystem
+    if(($letter -ne $null) -and ($fileSystem -ne $null)){
+        $drives = $drives + $letter
+        $totalCapacity += $volume.Capacity
+        $totalFree += $volume.FreeSpace
+    }
+}
+
+$totalCapacity = ($totalCapacity/1gb)
+$totalFree = ($totalFree/1gb)
+Write-Host "Volumes:`t $drives"
+Write-Host "Total Capacity(GB) `t $totalCapacity"
+Write-Host "Total Free(GB) `t $totalFree"
